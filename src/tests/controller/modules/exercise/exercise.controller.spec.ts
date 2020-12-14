0830969be72fe24da3252cc2c24cab0d10cf7f66
Johannes Henrich <Johannes.Henrich@gmail.de>
2020-12-14 13:33:40 +0100

    Added first testfile

---
+++ src/tests/controller/modules/exercise/exercise.controller.spec.ts
@@ -0,0 +1,9 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ExerciseController } from '../../../../modules/exercise/exercise.controller';
+import { ExerciseService } from '../../../../modules/exercise/exercise.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Exercise } from '../../../../modules/exercise/schemas/exercise.schema';
+
+describe('ExerciseController', () => {
+
+});
